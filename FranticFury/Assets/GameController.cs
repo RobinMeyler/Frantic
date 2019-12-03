8c776607c7294928ca265e5002ec867759e6a0ef
RobinMeyler <c00231699@itcarlow.ie>
2019-12-03 13:16:46 +0000

    Plats

---
+++ FranticFury/Assets/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static float dropSpeed;
+    public GameObject platform;
+    public List<GameObject> platforms = new List<GameObject>();
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropSpeed = 0.001f;
+        for (int i = 0; i < 7; i++)
+        {
+            Quaternion spawnRotation = Quaternion.identity;
+            float randX = Random.Range(-2.34f, 2.34f);
+            Vector3 spawnPosition = new Vector3(randX,5 -(i*1.6f), 0);
+            Instantiate(platform, spawnPosition, spawnRotation);
+            platforms.Add(platform);
+        }
+
+        Debug.Log(platforms.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(platforms.Count);
+        //foreach (GameObject G in platforms)
+        //{
+        //    if (G.transform.position.y < -5.5) 
+        //    {
+        //        Destroy(G);
+        //        platforms.Remove(G);
+        //        Quaternion spawnRotation = Quaternion.identity;
+        //        float randX = Random.Range(-2.34f, 2.34f);
+        //        Vector3 spawnPosition = new Vector3(randX, 5.5f, 0);
+        //        Instantiate(platform, spawnPosition, spawnRotation);
+        //        platforms.Add(platform);
+        //    }
+        //}
+    }
+}
---
+++ FranticFury/Assets/platformBehav.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformBehav : MonoBehaviour
+{
+    public float dropSpeed = 0;
+    public float sideSpeed = 0;
+    public float time = 0;
+    int oop = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropSpeed = -0.01f;
+        sideSpeed = 0.01f;
+
+
+    // Random start x logic
+    // start y above screen
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Side swaping logic
+        // Drop speed increasing logic
+        // Once below screen reset
+        if(transform.position.x > 2.34 || transform.position.x < -2.34)
+        {
+            sideSpeed = -sideSpeed;
+        }
+        if (transform.position.y < -5.9)
+        {
+            float randX = Random.Range(-2.34f, 2.34f);
+         
+            transform.position = new Vector3(randX, 5.5f, 0);
+        }
+
+        time += Time.deltaTime;
+
+        if (time > 5)
+        {
+            Debug.Log(++oop);
+            time = 0;
+            dropSpeed = dropSpeed - 0.001f;
+        }
+        transform.position = transform.position + new Vector3(sideSpeed, dropSpeed, 0);
+    }
+}
