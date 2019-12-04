4e1b552d2214b3a55d7e11ba3fcf1dad23f93d1b
RobinMeyler <c00231699@itcarlow.ie>
2019-12-04 10:51:37 +0000

    Menu added

---
+++ FranticFury/Assets/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayButton : MonoBehaviour
+{
+    public Button thisButton;
+    AudioSource clickObj;
+
+    void Start()
+    {
+        Button btn = thisButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        //GameObject uiClick = GameObject.FindGameObjectWithTag("ClickFX");
+        //clickObj = uiClick.GetComponent<AudioSource>();
+    }
+
+    void TaskOnClick()
+    {
+        //clickObj.Play();
+        SceneManager.LoadScene("FranticFury", LoadSceneMode.Single);
+    }
+}
