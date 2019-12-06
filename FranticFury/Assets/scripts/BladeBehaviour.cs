396d5381be55f4db87bf38b73f332b5fdad20d03
JoshTyrrellBrowne <c00224660@itcarlow.com>
2019-12-06 12:56:59 +0000

    blade movement

---
+++ FranticFury/Assets/scripts/BladeBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BladeBehaviour : MonoBehaviour
+{
+    private GameObject platform;
+    public GameObject gameController;
+    GameObject[] platforms;
+    private float degrees = 0f;
+    private int index;
+    private float offset = 0f;
+    private bool movingDir = true; //true == right, false == left
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platforms = GameObject.FindGameObjectsWithTag("Platform");
+        index = Random.Range(0, platforms.Length);
+        this.transform.position = platforms[index].transform.position;
+        this.transform.parent = platforms[index].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingDir)
+        {
+            offset = offset + 0.01f;
+        }
+        else
+        {
+            offset = offset - 0.01f;
+        }
+        if (offset > 0.5)
+        {
+            movingDir = false;
+        }
+        if (offset < -0.5)
+        {
+            movingDir = true;
+        }
+        this.transform.position = new Vector3(platforms[index].transform.position.x + offset, this.transform.position.y, this.transform.position.z);
+
+
+        degrees += 1;
+        this.transform.eulerAngles = Vector3.forward * degrees;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+    }
+}
