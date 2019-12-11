40ce0f6e80492669302a760917e9af2662686bc6
RobinMeyler <c00231699@itcarlow.ie>
2019-12-11 09:21:59 +0000

    Game over

---
+++ FranticFury/Assets/MenuButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public Button thisButton;
+    AudioSource clickObj;
+
+    void Start()
+    {
+        Button btn = thisButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        //GameObject uiClick = GameObject.FindGameObjectWithTag("ClickFX");
+        //clickObj = uiClick.GetComponent<AudioSource>();
+    }
+
+    void TaskOnClick()
+    {
+        //clickObj.Play();
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
